69cf140152a21462cc8526cbf3c1d96d168fe161
Dmitrii Holubtsov <dmitrii.h@ideasoft.io>
2023-02-22 18:16:57 +0200

    Data Persistance with API Request

--- src/lessons/HandleForms/index.js
+++ src/lessons/HandleForms/index.js
@@ -1,14 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+import axios from 'axios';
 
 const Books = () => {
     const [books, setBooks] = useState([]);
 
-    const editBookById = (id, newTitle) => {
+  
+
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+
+        setBooks(response.data);
+    };
+    // Use this
+    useEffect(() => {
+        fetchBooks();
+    },[]);
+
+    // Don't do this
+    //fetchBooks();
+ 
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle
+        })
+
         const updatedBooks = books.map((book) => {
             if (book.id === id) {
-                return { ...book, title: newTitle };
+                return { ...book, ...response.data };
             }
 
             return book;
@@ -17,7 +37,8 @@ const Books = () => {
         setBooks(updatedBooks);
     };
     
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+       await axios.delete(`http://localhost:3001/books/${id}`);
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
         });
@@ -25,13 +46,18 @@ const Books = () => {
         setBooks(updatedBooks);
     };
     
-    const handleCreateBook = (title) => {
+    const handleCreateBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title,
+        })
         const updatedBooks = [
             ...books,
-            { id: Math.round(Math.random() * 9999), title }
+           response.data
         ];
         setBooks(updatedBooks);
     };
+
+    
 
     return (
         <div className="app">
